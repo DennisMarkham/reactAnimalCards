4d5cb57ac78891eb484def6a1ae7a04ab909da48
unknown <workdennis@yahoo.com>
2019-11-04 19:32:22 -0600

    basic functionality works, but problem changing styles.  Why?

--- src/components/animal.js
+++ src/components/animal.js
@@ -1,16 +1,46 @@
 import React from "react";
+import animals from "./animals.json";
 
-const animal = props => (
-  <div className="animal">
-    <h2>{props.animal}</h2>
-    <h3>What are {props.animal} made of?</h3>
+class Animal extends React.Component
+{
+
+	 state = {
+    count: 0
+  };
+
+  handleWolves = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ count: 0});
+  };
+
+
+  handleCheetahs = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ count: 1});
+  };
+
+
+  handleOwls = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ count: 2});
+  };
+
+render() {return (
+<div><button onClick={this.handleWolves}>Wolves</button>
+<button onClick={this.handleCheetahs}>Cheetahs</button><button onClick={this.handleOwls}>Owls</button>
+  <div style={{color:"{animals[this.state.count].color}"}}>
+    <h2>{animals[this.state.count].animal}</h2>
+    <h3>What are {animals[this.state.count].animal} made of?</h3>
     <ol>
-    <li>{props.l1}</li>
-    <li>{props.l2}</li>
-    <li>{props.l3}</li>
+    <li>{animals[this.state.count].l1}</li>
+    <li>{animals[this.state.count].l2}</li>
+    <li>{animals[this.state.count].l3}</li>
     </ol>
   </div>
+  </div>
 );
+}
+}
 
-export default animal;
+export default Animal;
 
